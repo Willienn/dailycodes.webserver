fd953e84e90946665768d29429aff2faaba61834
Willien <willien_dev@outlook.com>
2024-10-17 15:14:56 -0300

    feat(main): change hidden to && operator

--- src/components/server-status.tsx
+++ src/components/server-status.tsx
@@ -10,18 +10,17 @@ export default function ServerStatus() {
   const [motd, setMotd] = useState<string | null>(null);
   const [icon, setIcon] = useState<string | null>(null);
 
-  // Fetch server status on component mount
   useEffect(() => {
     async function fetchServerStatus() {
       try {
         const response = await fetch(`${API_BASE_URL}${SERVER_URL}`);
         const data = await response.json();
         console.log(data);
+
         if (data.online) {
           setStatus("Online");
           setPlayers(`${data.players.online} / ${data.players.max}`);
           setIcon(data.icon);
-
           setMotd(data.motd.clean.join(""));
         } else {
           setStatus("Offline");
@@ -42,31 +41,21 @@ export default function ServerStatus() {
     <div className="p-4 bg-gray-800 rounded-lg text-white">
       <h2 className="text-2xl font-bold mb-2">Server Status</h2>
 
-      <div className="flex  gap-2">
-        <img
-          src={icon}
-          alt="Server Icon"
-          className={`${!icon && "hidden"} w-16 h-16`}
-        />
+      <div className="flex gap-2">
+        {icon && <img src={icon} alt="Server Icon" className="w-16 h-16" />}
+
         <div className="flex flex-col gap-2">
           <p className="flex justify-between">
-            <>
-              <span
-                className={`${status !== "Online" && "hidden"} text-green-500`}
-              >
-                Online
-              </span>
-              <span
-                className={`${status !== "Offline" && "hidden"} text-red-500`}
-              >
-                Offline
-              </span>
-            </>
-            <span className={`${!players && "hidden"}`}> {players}</span>
+            {status === "Online" ? (
+              <span className="text-green-500">Online</span>
+            ) : (
+              <span className="text-red-500">Offline</span>
+            )}
+
+            {players && <span>{players}</span>}
           </p>
-          <div className={`${!motd && "hidden"} text-sm text-gray-300`}>
-            {motd}
-          </div>
+
+          {motd && <div className="text-sm text-gray-300">{motd}</div>}
         </div>
       </div>
     </div>
